81809e7e253a22cb5a9c16b3cdc7cb829f349be4
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-04-01 20:11:25 -0300

    lidando com pacotes externos

--- 01-Pacotes/main.go
+++ 01-Pacotes/main.go
@@ -3,10 +3,15 @@ package main
 import(
 	"modulo/auxiliar" 
 	"fmt"
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main.go")
 	auxiliar.Escrever()
 	//auxiliar.escrever2() // Não compila, pois a função escrever2 não é exportada
+
+	erro := checkmail.ValidateFormat("marianosilvagmail.com")
+
+	fmt.Println(erro)
 }
