0ea8cb47b8c0797c3b640d58ecb95997a4441f85
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-04-22 20:14:02 -0300

    lidando com json em go

--- 22-JSON/main.go
+++ 22-JSON/main.go
@@ -15,15 +15,26 @@ type cachorro struct {
 
 func main(){
 	//json.Marshal() // converte map pra json ou struct pra json
-	//json.Unmarshal()//transforma json em sruct ou map dependendo da necessidadade
+	
 
-	c := cachorro{"Rex", "Dalmata", 3}
-	cJson, error := json.Marshal(c)
+	cachorro1 := cachorro{"Rex", "Dalmata", 3}
+	cachorroJson, error := json.Marshal(cachorro1)
 
 	if(error != nil){
 		log.Fatal(error)
 	}
 
-	fmt.Println(cJson) // vai logar um slice de byte
-	fmt.Println(bytes.NewBuffer(cJson)) //com o uso da lib bytes consigo vizualizar o json {"nome":"Rex","raca":"Dalmata","idade":3}
+	fmt.Println(cachorroJson) // vai logar um slice de byte
+	fmt.Println(bytes.NewBuffer(cachorroJson)) //com o uso da lib bytes consigo vizualizar o json {"nome":"Rex","raca":"Dalmata","idade":3}
+
+
+	//json.Unmarshal()//transforma json em sruct ou map dependendo da necessidadade
+
+	var cachorro2 cachorro //declara a struct em branco
+	if erro := json.Unmarshal(cachorroJson, &cachorro2); erro != nil{ //recebe um slice de byte que seria o json e um endereço de memoria onde vai colocar a conversao
+		log.Fatal(erro)
+	} 
+
+	fmt.Println(cachorro2)
+
 }
