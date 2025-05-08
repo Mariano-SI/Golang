a64e4080e60a2b45a6bf2118885bf22abe497313
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-05-07 21:21:58 -0300

    tipos primitivos, compostos e valores zero

---
+++ AprendaGo/02-tipos.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	//tipos em go sao estaticos ou seja se uma variavel tem tipo x ela sempre sera do tipo x. 
+
+	//Tipos primitivos dentro da linguagem go: disponíveis na linguagem nativamente como blocos básicos de construção
+	// -int, string, bool
+
+	//Tipos de dados compostos: são tipos compostos de tipos primitivos, e criados pelo usuário
+    //slice, array, struct, map
+
+	//valores zero
+	/* ints: 0
+	floats: 0.0
+	booleans: false
+	strings: ""
+	pointers, functions, interfaces, slices, channels, maps: nil
+	Use := sempre que possível.
+	Use var para package-level scope. */
+}
