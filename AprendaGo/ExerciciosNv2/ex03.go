5c7fa1d57879882590bc6be37a874de92a6f0432
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-05-09 19:47:53 -0300

    ex03

---
+++ AprendaGo/ExerciciosNv2/ex03.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		Crie constantes tipadas e não-tipadas.
+		Demonstre seus valores.
+	*/
+
+	const i = "aaa"
+	const a int = 15
+
+	fmt.Println(i)
+	fmt.Println(a)
+}
