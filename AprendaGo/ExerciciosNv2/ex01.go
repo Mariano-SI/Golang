4e993add9f1569fbf0eb5a1321756f421ad888dc
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-05-09 19:41:00 -0300

    ex 01 nv 2

---
+++ AprendaGo/ExerciciosNv2/ex01.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	//escreva um programa que mostre um numero em decimal, binario e hexadecimal
+
+	num := 1212
+	fmt.Printf("%v\n%b\n%X", num, num, num)
+}
