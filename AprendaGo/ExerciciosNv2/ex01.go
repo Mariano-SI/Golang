76337cb5657f333bf39c607e4f2e0ca48e59d4ff
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-05-09 19:41:24 -0300

    ex 01 corrigido

--- AprendaGo/ExerciciosNv2/ex01.go
+++ AprendaGo/ExerciciosNv2/ex01.go
@@ -6,5 +6,5 @@ func main() {
 	//escreva um programa que mostre um numero em decimal, binario e hexadecimal
 
 	num := 1212
-	fmt.Printf("%v\n%b\n%X", num, num, num)
+	fmt.Printf("%v\n%b\n%x", num, num, num)
 }
