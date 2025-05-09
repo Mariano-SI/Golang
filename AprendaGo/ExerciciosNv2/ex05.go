e44a91704f58542f2199efeebfab011e7255ca3f
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-05-09 19:53:52 -0300

    ex05

---
+++ AprendaGo/ExerciciosNv2/ex05.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* 
+	Crie uma variável de tipo string utilizando uma raw string literal.
+	Demonstre-a.
+	*/
+
+	myString := `aaaaa
+	
+	bbbbb
+			cccccc
+	`
+
+	fmt.Println(myString)
+}
