eaf56a94db0d4f1707f1efd7aaa6581c6e3fa661
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-05-14 21:03:06 -0300

    ex06

---
+++ AprendaGo/ExerciciosNv3/ex06.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Crie um programa que demonstre o funcionamento da declaração if.
+
+	idade := 24
+	if idade < 30 {
+		fmt.Println("Ainda nao chegou aos 30")
+	}
+}
